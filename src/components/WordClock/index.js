af84150fb1695291aae8984a57c2427dae4efabf
Jason Manson-Hing <jason.mansonhing@gmail.com>
2020-11-08 04:08:33 -0500

    added date and time animations

--- src/components/WordClock/index.js
+++ src/components/WordClock/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import getTimeApproximation from '../../utils/time';
 import getDateText from '../../utils/date';
 import styled from 'styled-components';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Container = styled.div`
   text-align: center;
@@ -20,6 +20,42 @@ const DateText = styled.h2`
   color: ${({ theme }) => theme.text};
 `
 
+const timeVariants = {
+  hidden: {
+    position: 'absolute',
+    opacity: 0,
+    y: 48,
+  },
+  visible: {
+    position: 'relative',
+    opacity: 1,
+    y: 0,
+  },
+  exit: {
+    position: 'absolute',
+    opacity: 0, 
+    y: -48,
+  }
+}
+
+const dateVariants = {
+  hidden: {
+    position: 'absolute',
+    opacity: 0,
+    y: 32,
+  },
+  visible: {
+    position: 'relative',
+    opacity: 1,
+    y: 0,
+  },
+  exit: {
+    position: 'absolute',
+    opacity: 0, 
+    y: -32,
+  }
+}
+
 const WordClock = ({ rounding = 'conservative', shortDate = false }) => {
   const [dateTime, setDateTime] = useState(new Date());
 
@@ -35,8 +71,47 @@ const WordClock = ({ rounding = 'conservative', shortDate = false }) => {
 
   return (
     <Container>
-      <TimeText>{ getTimeApproximation(dateTime, rounding) }</TimeText>
-      <DateText>{ getDateText(dateTime, shortDate) }</DateText>
+      <TimeText>
+        <AnimatePresence>
+          { getTimeApproximation(dateTime, rounding).split(/(\s+)/).map( (word, index) => {
+            return (
+              <motion.span
+                key={`${word}-${index}`}
+                style={{ display: 'inline-block', lineHeight: '1.5', whiteSpace: 'pre' }}
+                variants={timeVariants}
+                initial='hidden'
+                animate='visible'
+                exit='exit'
+                transition={{
+                  duration: 0.75,
+                  ease: [0, 0.05, 0.36, 1.0]
+                }}
+              >
+                {word}
+              </motion.span>
+            )
+            }) 
+          }
+        </AnimatePresence>
+      </TimeText>
+      <DateText>
+        <AnimatePresence>
+          <motion.span
+            key={getDateText(dateTime, shortDate)}
+            style={{ display: 'inline-block', lineHeight: '1.5', whiteSpace: 'pre' }}
+            variants={dateVariants}
+            initial='hidden'
+            animate='visible'
+            exit='exit'
+            transition={{
+              duration: 0.75,
+              ease: [0, 0.05, 0.36, 1.0]
+            }}
+          >
+            {getDateText(dateTime, shortDate)}
+          </motion.span>
+        </AnimatePresence>
+      </DateText>
     </Container>
   )
 }
